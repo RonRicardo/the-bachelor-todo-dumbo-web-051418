00ab70978eeff02830ac15256fcf49e7aa20c73e
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-03 17:40:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,6 +37,7 @@ def get_average_age_for_season(data, season)
      ages = contestants.collect do |contestant|
         contestant["age"].to_i
     end
+    binding.py
     return (ages.reduce(:+)/contestants.size)
   end
 end
