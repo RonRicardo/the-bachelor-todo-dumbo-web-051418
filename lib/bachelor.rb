2c63412fee192f1a3fd103059213abc79e73013c
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-03 17:45:04 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -33,7 +33,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  data.each do |season, contestants|
+  data[season].each do
      ages = contestants.collect do |contestant|
         contestant["age"].to_i
     end
