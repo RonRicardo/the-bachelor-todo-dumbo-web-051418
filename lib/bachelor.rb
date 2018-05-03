a3ca945a8187144e85264fc6e270eec0c5f799b0
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-03 17:35:18 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,42 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+      return contestant["name"].split(" ")[0] if contestant["status"].include?('Winner')
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      return contestant["name"] if contestant["occupation"] == (occupation)
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      hometown_count += 1 if contestant["hometown"] == hometown
+    end
+  end
+  hometown_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      return contestant["occupation"] if contestant["hometown"] == hometown
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data.each do |season, contestants|
+     ages = contestants.collect do |contestant|
+        contestant["age"].to_i
+    end
+    return (ages.reduce(:+)/contestants.size)
+  end
 end
